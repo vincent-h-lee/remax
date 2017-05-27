813df54670b9555f4f38126fc304d8ae9009f7f9
Vincent Lee <vincent.h.lee@outlook.com>
2017-05-27 12:49:11 -0700

    move data processing out from records class

--- data.cpp
+++ data.cpp
@@ -28,7 +28,7 @@ void Data::parseData(std::string& data) {
 		}
 	}
 }
-
+/* Indexing */
 void Data::removeAtIndex(std::vector<size_t> indices) {
 	int i = indices.size() -1; 
 	while(i > -1) {
@@ -50,10 +50,55 @@ size_t Data::findIndex(std::string data) {
 	return (size_t) -1;
 }
 
+std::string Data::getAtIndex(const size_t index) {
+	return data_[index];
+}
+
+//make this static
+/* Data processing*/
 std::string Data::withQuotes(const std::string& text) {
 	return '"' + text + '"';
 }
 
+std::map<std::string, std::string> Data::getPhonebook(std::string& list) {
+	std::string nametoken = ""; 
+	std::string phonetoken = "";
+	bool onname = true; 
+	std::map<std::string, std::string> phonebook; 
+	std::string fixlist = list; //TODO remove the quotations if they exist 
+	for(auto& ch:fixlist) {
+		if(onname) {
+			if(ch == ':') {
+				onname = false;
+			} else {
+				nametoken+=ch; 
+			}
+		} else {
+			if(ch == ';') {
+				onname = true; 
+				phonebook[nametoken] = phonetoken; //TODO Trim whitespace
+				nametoken = "";
+				phonetoken = ""; 
+			} else {
+				phonetoken+=ch; 
+			}
+		}
+	}
+	return phonebook; 
+}
+
+std::vector<std::string> Data::getNames(std::string& name) {
+	std::vector<std::string> names; 
+	std::istringstream iss(name); 
+	std::string temp;
+	while(iss >> temp) {
+		names.push_back(temp); 
+		temp = "";
+	}
+	return names;
+}
+
+/* Output */
 void Data::print() {
 	for(auto it=data_.begin(); it!=data_.end(); ++it) {
 		std::cout << *it << " " << std::endl;
--- data.h
+++ data.h
@@ -11,11 +11,17 @@ public:
 	Data(std::string& data) {
 		parseData(data);
 	}
+	Data(const Data& rhs) {
+		data_ = rhs.data_;
+	}
 	void parseData(std::string& data); 
 	void removeAtIndex(std::vector<size_t> indices);
 	void replaceAtIndex(const size_t index, const std::string& text);
 	size_t findIndex(std::string data);
+	std::string getAtIndex(const size_t index);
 	std::string withQuotes(const std::string& text);
+	std::map<std::string, std::string> getPhonebook(std::string& list); 
+	std::vector<std::string> getNames(std::string& name); //first, last, company 		
 	void print(); 
 	void output();
 private: 
